1f3d357f1a89176bd93a5c10b44089432618bf64
Raka Wiradinata <rakawiradinata@gmail.com>
2018-06-09 15:52:23 -0400

    modified

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -2,6 +2,20 @@ import { clientID, clientSecret} from './Secret.js.gpg';
 import React from 'react';
 
 class Spotify extends React.Component {
+  search(q, type) {
+    const corsUrl = 'https://www.cors-anywhere.herokuapp.com/';
+    const url = 'https://api.spotify.com';
+    const fullUrl = `${corsUrl}${url}`;
+    return fetch(fullUrl, {}
+    ).then( response =>
+      return response.json();
+    ).then(jsonResponse =>
+      if(jsonResponse === PLACEHOLDER) { //PLACEHOLDER
+        return 'HELLO'; //PLACEHOLDER
+      }
+    );
+  }
+
   render() {
     return (
       <h1>hi</h1>
